91e99eeb850456ce6e2fe2896c42d0ab4ee0d073
Azmath-007 <mohammedazmathullaharshad@gmail.com>
2024-07-24 18:55:09 -0400

    Create priority_queue_example.py

---
+++ priority_queue_example.py
@@ -0,0 +1,13 @@
+1) my_priority_queue = queue.PriorityQueue()
+
+2) my_priority_queue.put((3, 'Apple'))
+
+    my_priority_queue.put((1, 'Banana'))
+
+    my_priority_queue.put((2, 'Cherry'))
+
+3) popped_element = my_priority_queue.get()
+
+4) print("Popped element:", popped_element)
+
+5) Popped element: (1, 'Banana')
